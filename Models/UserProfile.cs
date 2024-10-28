778b0eb12c64f6f70ff3c97f45ffe1da599ddf8d
Jilliane Rhaine <Jilliane Rhaine@LAPTOP-UPB4S8G5>
2024-10-28 22:19:50 +0800

    Add project files.

---
+++ Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using FontaineAdmin.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace FontaineAdmin.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        [Required]
+        public string UniqueUserName { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string Type { get; set; }
+    }
+}
